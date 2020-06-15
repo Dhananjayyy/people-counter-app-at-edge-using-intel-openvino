bdc0827ee06ef71192a2491374ed09341d397b79
Dhananjay Yelwande <36818729+Dhananjayyy@users.noreply.github.com>
2020-06-15 15:59:17 +0530

    Update inference.py

--- inference.py
+++ inference.py
@@ -21,10 +21,10 @@
 import os
 import sys
 import logging as log
+
 from openvino.inference_engine import IENetwork, IECore
 
-
-
+DEVICE = 'CPU'
 class Network:
     """
     Load and configure inference plugins for the specified target devices 
@@ -40,68 +40,58 @@ class Network:
         self.exec_network = None
         self.infer_request = None
 
-    
-    def load_model(self, model, CPU_EXTENSION, DEVICE, console_output= False):
+    def load_model(self, model_xml, CPU_EXTENSION, DEVICE):
         ### TODO: Load the model ###
-        model_xml = model
         model_bin = os.path.splitext(model_xml)[0] + ".bin"
         
         self.plugin = IECore()
         self.network = IENetwork(model=model_xml, weights=model_bin)
+        
         ### TODO: Check for supported layers ###
-        if not all_layers_supported(self.plugin, self.network, console_output=console_output):
+        layers_supported = self.plugin.query_network(self.network, DEVICE)
+        layers = self.network.layers.keys()
+
+        total_supported = True
+        for i in layers:
+            if i not in layers_supported:
+                total_supported = False
+        if not total_supported:
+            print('Not all model layers supported, please specify an appropriate cpu_extension with --cpu_extension')
             self.plugin.add_extension(CPU_EXTENSION, DEVICE)
             
         self.exec_network = self.plugin.load_network(self.network, DEVICE)
+        
         # Get the input layer
         self.input_blob = next(iter(self.network.inputs))
         self.output_blob = next(iter(self.network.outputs))
-       
+
         ### TODO: Return the loaded inference plugin ###
         ### Note: You may need to update the function parameters. ###
-        return
+        return 
+    
 
     def get_input_shape(self):
         ### TODO: Return the shape of the input layer ###
-        input_shapes = {}
-        for inp in self.network.inputs:
-            input_shapes[inp] = (self.network.inputs[inp].shape)
-        return input_shapes
+        input_shape = self.network.inputs[self.input_blob].shape
+        return input_shape
     
 
-    def exec_net(self, net_input, request_id):
+    def exec_net(self, request_id, net_input):
         ### TODO: Start an asynchronous request ###
         ### TODO: Return any necessary information ###
-        self.infer_request_handle = self.exec_network.start_async(
-                request_id, 
-                inputs=net_input)
+        execute_net = self.infer_request_handle = self.exec_network.start_async(request_id, net_input)
 
-        return 
+        return execute_net
 
 
     def wait(self):
         ### TODO: Wait for the request to be complete. ###
         ### TODO: Return any necessary information ###
         ### Note: You may need to update the function parameters. ###
-        status = self.infer_request_handle.wait()
-        return status
+        return self.infer_request_handle.wait()
     
 
     def get_output(self):
         ### TODO: Extract and return the output results
         ### Note: You may need to update the function parameters. ###
-        out = self.infer_request_handle.outputs[self.output_blob]
-        return out
-
-def all_layers_supported(engine, network, console_output=False):
-    ### TODO check if all layers are supported
-    ### return True if all supported, False otherwise
-    layers_supported = engine.query_network(network, device_name='CPU')
-    layers = network.layers.keys()
-
-    all_supported = True
-    for l in layers:
-        if l not in layers_supported:
-            all_supported = False
-
-    return all_supported
+        return self.infer_request_handle.outputs[self.output_blob]
